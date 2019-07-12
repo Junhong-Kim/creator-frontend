161ddbaa67e1db513544285beb9d74ac1a254823
Junhong-Kim <jhk1005jhk@gmail.com>
2019-07-13 00:14:22 +0900

    추가: 네비게이션, 로그인 메뉴

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -6,20 +6,31 @@
       fixed app
     >
       <v-list>
+        <v-subheader>크리에이터</v-subheader>
         <v-list-tile @click="$router.push({name: 'home'})">
           <v-list-tile-action>
-            <v-icon>home</v-icon>
+            <v-icon>trending_up</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title>Home</v-list-tile-title>
+            <v-list-tile-title>YouTube 랭킹</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
+        <v-subheader>에디터</v-subheader>
         <v-list-tile @click="$router.push({name: 'about'})">
           <v-list-tile-action>
-            <v-icon>contact_mail</v-icon>
+            <v-icon>assignment_ind</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title>Contact</v-list-tile-title>
+            <v-list-tile-title>채용공고</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-subheader>커뮤니티</v-subheader>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>list</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>자유게시판</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
@@ -30,7 +41,56 @@
     >
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title>Application</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <!-- after login -->
+      <v-menu
+        v-if="isLogin"
+        offset-y
+        transition="slide-y-transition"
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>face</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-content>
+              <v-list-tile-title>내 정보</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile>
+            <v-list-tile-content>
+              <v-list-tile-title>내 글관리</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-divider></v-divider>
+          <v-list-tile @click="logout">
+            <v-list-tile-content>
+              <v-list-tile-title>로그아웃</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+      <!-- before login -->
+      <v-menu
+        v-else
+        offset-y
+        transition="slide-y-transition"
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>person</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-tile @click="login">
+            <v-list-tile-title>회원가입 | 로그인</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
     </v-toolbar>
+    <!-- content -->
     <v-content>
       <v-container fluid fill-height>
         <v-layout
@@ -52,5 +112,14 @@ import { Component, Vue } from 'vue-property-decorator';
 @Component
 export default class Main extends Vue {
   drawer: boolean = false;
+  isLogin: boolean = false;
+
+  login() {
+    this.isLogin = true;
+  }
+
+  logout() {
+    this.isLogin = false;
+  }
 }
 </script>
