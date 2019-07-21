3f960a70d36ec619c267dc4fdfee74542678111b
Junhong-Kim <jhk1005jhk@gmail.com>
2019-07-21 20:15:23 +0900

    추가: build 설정

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   devServer: {
     proxy: {
@@ -7,4 +9,6 @@ module.exports = {
       },
     },
   },
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  outputDir: path.resolve(__dirname, '../backend/static'),
 }
