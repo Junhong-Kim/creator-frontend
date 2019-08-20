fd5ec8ed5a0278aa19caec52c815f718e72016c7
Junhong-Kim <jhk1005jhk@gmail.com>
2019-08-20 23:55:02 +0900

    수정: tiptap 텍스트 에디터 스타일

--- src/views/TextEditor.vue
+++ src/views/TextEditor.vue
@@ -1,122 +1,141 @@
 <template>
-  <div>
-    <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
-      <div>
-        <button
-          :class="{ 'is-active': isActive.bold() }"
-          @click="commands.bold"
-        >
-          bold&nbsp;
-        </button>
+  <v-layout column class="elevation-2">
+    <v-flex grey lighten-3 pa-2>
+      <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
+        <div>
+          <v-btn
+            icon
+            :class="{ 'is-active': isActive.bold() }"
+            @click="commands.bold"
+          >
+            <v-icon color="black">format_bold</v-icon>
+          </v-btn>
 
-        <button
-          :class="{ 'is-active': isActive.italic() }"
-          @click="commands.italic"
-        >
-          italic&nbsp;
-        </button>
+          <v-btn
+            icon
+            :class="{ 'is-active': isActive.italic() }"
+            @click="commands.italic"
+          >
+            <v-icon color="black">format_italic</v-icon>
+          </v-btn>
 
-        <button 
-          :class="{ 'is-active': isActive.strike() }"
-          @click="commands.strike"
-        >
-          strike&nbsp;
-        </button>
+          <v-btn
+            icon
+            :class="{ 'is-active': isActive.strike() }"
+            @click="commands.strike"
+          >
+            <v-icon color="black">format_strikethrough</v-icon>
+          </v-btn>
 
-        <button
-          :class="{ 'is-active': isActive.underline() }"
-          @click="commands.underline"
-        >
-          underline&nbsp;
-        </button>
+          <v-btn
+            icon
+            :class="{ 'is-active': isActive.underline() }"
+            @click="commands.underline"
+          >
+            <v-icon color="black">format_underline</v-icon>
+          </v-btn>
 
-        <button
-          :class="{ 'is-active': isActive.code() }"
-          @click="commands.code"
-        >
-          code&nbsp;
-        </button>
+          <v-btn
+            icon
+            :class="{ 'is-active': isActive.code() }"
+            @click="commands.code"
+          >
+            <v-icon color="black">code</v-icon>
+          </v-btn>
 
-        <button
-          :class="{ 'is-active': isActive.paragraph() }"
-          @click="commands.paragraph"
-        >
-          paragraph&nbsp;
-        </button>
+          <v-btn
+            icon
+            :class="{ 'is-active': isActive.paragraph() }"
+            @click="commands.paragraph"
+          >
+            <span class="title">P</span>
+          </v-btn>
 
-        <button
-          :class="{ 'is-active': isActive.heading({ level: 1 }) }"
-          @click="commands.heading({ level: 1 })"
-        >
-          H1&nbsp;
-        </button>
+          <v-btn
+            icon
+            :class="{ 'is-active': isActive.heading({ level: 1 }) }"
+            @click="commands.heading({ level: 1 })"
+          >
+            <span class="title">H1</span>
+          </v-btn>
 
-        <button
-          :class="{ 'is-active': isActive.heading({ level: 2 }) }"
-          @click="commands.heading({ level: 2 })"
-        >
-          H2&nbsp;
-        </button>
+          <v-btn
+            icon
+            :class="{ 'is-active': isActive.heading({ level: 2 }) }"
+            @click="commands.heading({ level: 2 })"
+          >
+            <span class="title">H2</span>
+          </v-btn>
 
-        <button
-          :class="{ 'is-active': isActive.heading({ level: 3 }) }"
-          @click="commands.heading({ level: 3 })"
-        >
-          H3&nbsp;
-        </button>
+          <v-btn
+            icon
+            :class="{ 'is-active': isActive.heading({ level: 3 }) }"
+            @click="commands.heading({ level: 3 })"
+          >
+            <span class="title">H3</span>
+          </v-btn>
 
-        <button
-          :class="{ 'is-active': isActive.bullet_list() }"
-          @click="commands.bullet_list"
-        >
-          bullet_list&nbsp;
-        </button>
+          <v-btn
+            icon
+            :class="{ 'is-active': isActive.bullet_list() }"
+            @click="commands.bullet_list"
+          >
+            <v-icon color="black">format_list_bulleted</v-icon>
+          </v-btn>
 
-        <button
-          :class="{ 'is-active': isActive.ordered_list() }"
-          @click="commands.ordered_list"
-        >
-          ordered_list&nbsp;
-        </button>
+          <v-btn
+            icon
+            :class="{ 'is-active': isActive.ordered_list() }"
+            @click="commands.ordered_list"
+          >
+            <v-icon color="black">format_list_numbered</v-icon>
+          </v-btn>
 
-        <button
-          :class="{ 'is-active': isActive.blockquote() }"
-          @click="commands.blockquote"
-        >
-          blockquote&nbsp;
-        </button>
+          <v-btn
+            icon
+            :class="{ 'is-active': isActive.blockquote() }"
+            @click="commands.blockquote"
+          >
+            <v-icon color="black">format_quote</v-icon>
+          </v-btn>
 
-        <button
-          :class="{ 'is-active': isActive.code_block() }"
-          @click="commands.code_block"
-        >
-          code_block&nbsp;
-        </button>
+          <v-btn
+            icon
+            :class="{ 'is-active': isActive.code_block() }"
+            @click="commands.code_block"
+          >
+            <v-icon color="black">code</v-icon>
+          </v-btn>
 
-        <button
-          @click="commands.horizontal_rule"
-        >
-          horizontal_rule&nbsp;
-        </button>
+          <v-btn
+            icon
+            @click="commands.horizontal_rule"
+          >
+            <v-icon color="black">remove</v-icon>
+          </v-btn>
 
-        <button
-          @click="commands.undo"
-        >
-          undo&nbsp;
-        </button>
+          <v-btn
+            icon
+            @click="commands.undo"
+          >
+            <v-icon color="black">undo</v-icon>
+          </v-btn>
 
-        <button
-          @click="commands.redo"
-        >
-          redo&nbsp;
-        </button>
-      </div>
-    </editor-menu-bar>
-    <editor-content
-      :editor="editor"
-      class="text-xs-left"
-    ></editor-content>
-  </div>
+          <v-btn
+            icon
+            @click="commands.redo"
+          >
+            <v-icon color="black">redo</v-icon>
+          </v-btn>
+        </div>
+      </editor-menu-bar>
+    </v-flex>
+    <v-flex text-xs-left pt-4 pb-3 pl-4 pr-4>
+      <editor-content
+        :editor="editor"
+      ></editor-content>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script lang="ts">
@@ -177,3 +196,7 @@ export default class TextEditor extends Vue {
   }
 }
 </script>
+
+<style>
+*:focus {outline:none}
+</style>
