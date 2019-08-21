72520f5e92ea4d827a7427c05f6ac696bb9cbdee
Junhong-Kim <jhk1005jhk@gmail.com>
2019-08-21 22:10:32 +0900

    추가: tiptap 텍스트 에디터 메뉴바 툴팁

--- src/views/TextEditor.vue
+++ src/views/TextEditor.vue
@@ -3,130 +3,227 @@
     <v-flex grey lighten-3 pa-2>
       <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
         <div>
-          <v-btn
-            icon
-            :class="{ 'is-active': isActive.bold() }"
-            @click="commands.bold"
-          >
-            <v-icon color="black">format_bold</v-icon>
-          </v-btn>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                icon
+                :class="{ 'is-active': isActive.bold() }"
+                @click="commands.bold"
+                v-on="on"
+              >
+                <v-icon color="black">format_bold</v-icon>
+              </v-btn>
+            </template>
+            <span>Bold</span>
+          </v-tooltip>
 
-          <v-btn
-            icon
-            :class="{ 'is-active': isActive.italic() }"
-            @click="commands.italic"
-          >
-            <v-icon color="black">format_italic</v-icon>
-          </v-btn>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                icon
+                :class="{ 'is-active': isActive.italic() }"
+                @click="commands.italic"
+                v-on="on"
+              >
+                <v-icon color="black">format_italic</v-icon>
+              </v-btn>
+            </template>
+            <span>Italic</span>
+          </v-tooltip>
 
-          <v-btn
-            icon
-            :class="{ 'is-active': isActive.strike() }"
-            @click="commands.strike"
-          >
-            <v-icon color="black">format_strikethrough</v-icon>
-          </v-btn>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                icon
+                :class="{ 'is-active': isActive.strike() }"
+                @click="commands.strike"
+                v-on="on"
+              >
+                <v-icon color="black">format_strikethrough</v-icon>
+              </v-btn>
+            </template>
+            <span>Strike</span>
+          </v-tooltip>
 
-          <v-btn
-            icon
-            :class="{ 'is-active': isActive.underline() }"
-            @click="commands.underline"
-          >
-            <v-icon color="black">format_underline</v-icon>
-          </v-btn>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                icon
+                :class="{ 'is-active': isActive.underline() }"
+                @click="commands.underline"
+                v-on="on"
+              >
+                <v-icon color="black">format_underline</v-icon>
+              </v-btn>
+            </template>
+            <span>Underline</span>
+          </v-tooltip>
 
-          <v-btn
-            icon
-            :class="{ 'is-active': isActive.code() }"
-            @click="commands.code"
-          >
-            <v-icon color="black">code</v-icon>
-          </v-btn>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                icon
+                :class="{ 'is-active': isActive.code() }"
+                @click="commands.code"
+                v-on="on"
+              >
+                <v-icon color="black">code</v-icon>
+              </v-btn>
+            </template>
+            <span>Code</span>
+          </v-tooltip>
 
-          <v-btn
-            icon
-            :class="{ 'is-active': isActive.paragraph() }"
-            @click="commands.paragraph"
-          >
-            <span class="title">P</span>
-          </v-btn>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                icon
+                :class="{ 'is-active': isActive.paragraph() }"
+                @click="commands.paragraph"
+                v-on="on"
+              >
+                <span class="title">P</span>
+              </v-btn>
+            </template>
+            <span>Paragraph</span>
+          </v-tooltip>
 
-          <v-btn
-            icon
-            :class="{ 'is-active': isActive.heading({ level: 1 }) }"
-            @click="commands.heading({ level: 1 })"
-          >
-            <span class="title">H1</span>
-          </v-btn>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                icon
+                :class="{ 'is-active': isActive.heading({ level: 1 }) }"
+                @click="commands.heading({ level: 1 })"
+                v-on="on"
+              >
+                <span class="title">H1</span>
+              </v-btn>
+            </template>
+            <span>Header-1</span>
+          </v-tooltip>
 
-          <v-btn
-            icon
-            :class="{ 'is-active': isActive.heading({ level: 2 }) }"
-            @click="commands.heading({ level: 2 })"
-          >
-            <span class="title">H2</span>
-          </v-btn>
 
-          <v-btn
-            icon
-            :class="{ 'is-active': isActive.heading({ level: 3 }) }"
-            @click="commands.heading({ level: 3 })"
-          >
-            <span class="title">H3</span>
-          </v-btn>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                icon
+                :class="{ 'is-active': isActive.heading({ level: 2 }) }"
+                @click="commands.heading({ level: 2 })"
+                v-on="on"
+              >
+                <span class="title">H2</span>
+              </v-btn>
+            </template>
+            <span>Header-2</span>
+          </v-tooltip>
 
-          <v-btn
-            icon
-            :class="{ 'is-active': isActive.bullet_list() }"
-            @click="commands.bullet_list"
-          >
-            <v-icon color="black">format_list_bulleted</v-icon>
-          </v-btn>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                icon
+                :class="{ 'is-active': isActive.heading({ level: 3 }) }"
+                @click="commands.heading({ level: 3 })"
+                v-on="on"
+              >
+                <span class="title">H3</span>
+              </v-btn>
+            </template>
+            <span>Header-3</span>
+          </v-tooltip>
 
-          <v-btn
-            icon
-            :class="{ 'is-active': isActive.ordered_list() }"
-            @click="commands.ordered_list"
-          >
-            <v-icon color="black">format_list_numbered</v-icon>
-          </v-btn>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                icon
+                :class="{ 'is-active': isActive.bullet_list() }"
+                @click="commands.bullet_list"
+                v-on="on"
+              >
+                <v-icon color="black">format_list_bulleted</v-icon>
+              </v-btn>
+            </template>
+            <span>Bulleted list</span>
+          </v-tooltip>
 
-          <v-btn
-            icon
-            :class="{ 'is-active': isActive.blockquote() }"
-            @click="commands.blockquote"
-          >
-            <v-icon color="black">format_quote</v-icon>
-          </v-btn>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                icon
+                :class="{ 'is-active': isActive.ordered_list() }"
+                @click="commands.ordered_list"
+                v-on="on"
+              >
+                <v-icon color="black">format_list_numbered</v-icon>
+              </v-btn>
+            </template>
+            <span>Ordered list</span>
+          </v-tooltip>
 
-          <v-btn
-            icon
-            :class="{ 'is-active': isActive.code_block() }"
-            @click="commands.code_block"
-          >
-            <v-icon color="black">code</v-icon>
-          </v-btn>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                icon
+                :class="{ 'is-active': isActive.blockquote() }"
+                @click="commands.blockquote"
+                v-on="on"
+              >
+                <v-icon color="black">format_quote</v-icon>
+              </v-btn>
+            </template>
+            <span>Block quote</span>
+          </v-tooltip>
 
-          <v-btn
-            icon
-            @click="commands.horizontal_rule"
-          >
-            <v-icon color="black">remove</v-icon>
-          </v-btn>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                icon
+                :class="{ 'is-active': isActive.code_block() }"
+                @click="commands.code_block"
+                v-on="on"
+              >
+                <v-icon color="black">code</v-icon>
+              </v-btn>
+            </template>
+            <span>Code block</span>
+          </v-tooltip>
 
-          <v-btn
-            icon
-            @click="commands.undo"
-          >
-            <v-icon color="black">undo</v-icon>
-          </v-btn>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                icon
+                @click="commands.horizontal_rule"
+                v-on="on"
+              >
+                <v-icon color="black">remove</v-icon>
+              </v-btn>
+            </template>
+            <span>Horizontal line</span>
+          </v-tooltip>
 
-          <v-btn
-            icon
-            @click="commands.redo"
-          >
-            <v-icon color="black">redo</v-icon>
-          </v-btn>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                icon
+                @click="commands.undo"
+                v-on="on"
+              >
+                <v-icon color="black">undo</v-icon>
+              </v-btn>
+            </template>
+            <span>Undo</span>
+          </v-tooltip>
+
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                icon
+                @click="commands.redo"
+                v-on="on"
+              >
+                <v-icon color="black">redo</v-icon>
+              </v-btn>
+            </template>
+            <span>Redo</span>
+          </v-tooltip>
         </div>
       </editor-menu-bar>
     </v-flex>
