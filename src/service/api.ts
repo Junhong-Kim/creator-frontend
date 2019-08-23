ae39792249075e7dba02fd3a13aaa2d6d05a3ca0
Junhong-Kim <jhk1005jhk@gmail.com>
2019-08-23 11:46:30 +0900

    추가: 자유게시판 새글 등록

--- src/service/api.ts
+++ src/service/api.ts
@@ -2,4 +2,5 @@ export default {
   // LOGIN_STATUS: '/api/auth/login_status/',
   USER_ME: '/api/user/me',
   YOUTUBE_CHANNELS: '/api/youtube/channels',
+  POST_LIST: '/api/posts',
 };
--- src/views/QuillTextEditor.vue
+++ src/views/QuillTextEditor.vue
@@ -18,13 +18,13 @@
         >
         </v-text-field>
         <quill-editor
-          v-model="content"
+          v-model="contents"
           :options="editorOption"
           class="white elevation-2"
         ></quill-editor>
         <v-layout row mt-3>
           <v-flex>
-            <v-btn small round color="primary">등록</v-btn>
+            <v-btn small round color="primary" @click="createPost">등록</v-btn>
             <v-btn small round color="grey lighten-5" @click="$router.go(-1)">취소</v-btn>
           </v-flex>
         </v-layout>
@@ -35,6 +35,8 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import AxiosServie from '@/service/axios';
+import API from '@/service/api';
 import 'quill/dist/quill.core.css';
 import 'quill/dist/quill.snow.css';
 import 'quill/dist/quill.bubble.css';
@@ -52,9 +54,27 @@ export default class QuillTextEditor extends Vue {
     { text: '글쓰기', disabled: true },
   ];
   title: string = '';
-  content: string = '';
+  contents: string = '';
   editorOption: object = {
     placeholder: 'Contents..',
   };
+
+  createPost() {
+    AxiosServie.instance.post(API.POST_LIST, {
+      title: this.title,
+      contents: this.contents,
+      userId: this.$store.getters.user.id,
+    }).then(() => {
+      alert('새 글이 등록되었습니다.');
+    }).finally(() => {
+      this.$router.go(-1);
+    });
+  }
 }
 </script>
+
+<style>
+.ql-editor {
+  height: 300px;
+}
+</style>
